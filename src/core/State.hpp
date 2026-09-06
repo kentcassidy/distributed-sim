19d71f6825508286fa99dbd28062ec05f79db11f
Kent Cassidy Desktop <kent.d.cassidy@outlook.com>
2026-09-05 18:04:41 -0700

    UNCHECKED. First draft of State

--- src/core/State.hpp
+++ src/core/State.hpp
@@ -5,3 +5,15 @@ State (pos, vel, att, omega)
 Plain struct that gets integrated
 
 */
+#pragma once
+#include "Math.hpp"
+
+struct State {
+    Vec3 position;
+    Vec3 velocity;
+    Quaternion attitude;
+    double mass = 0.0;
+    Vec3 angularV;
+
+    //EntityID here or elsewhere?
+}
